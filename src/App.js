bfce5e7a4a84041f7323c3f34723cabf5991218a
DIPANKAR <you@example.com>
2022-12-16 17:58:04 +0600

    code updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,89 @@
-import logo from './logo.svg';
 import './App.css';
+import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth';
+import app from './firebase.init';
+import 'bootstrap/dist/css/bootstrap.css';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import { useState } from 'react';
+
+
+const auth = getAuth(app);
+
 
 function App() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [validated, setValidated] = useState(false);
+  const [error, setError] = useState('');
+
+  const handleEmailBlur = (event) => {
+    setEmail(event.target.value);
+  }
+  const handlePasswordBlur = (event) => {
+    setPassword(event.target.value);
+  }
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    //  validate code 
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.stopPropagation();
+      return;
+    }
+    // form validation with regular expression special character
+    if (!/(?=.*?[#?!@$%^&*-])/.test(password)) {
+      setError('Please should contain at least on special character');
+      return;
+    }
+    setError('');
+    setValidated(true);
+
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+
+    console.log('submit');
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='w-50 mx-auto mt-3'>
+      <h2 className="text-primary">Email Password Authentication</h2>
+      <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control onBlur={handleEmailBlur} type="email" placeholder="Enter email" required />
+          {/* validation error message */}
+          <Form.Control.Feedback type="invalid">
+            Please provide a valid email.
+          </Form.Control.Feedback>
+
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control onBlur={handlePasswordBlur} type="password" placeholder="Password" required />
+          {/* validation error message  */}
+          <Form.Control.Feedback type="invalid">
+            Please provide a valid password.
+          </Form.Control.Feedback>
+        </Form.Group>
+        <p className='text-danger'>{error}</p>
+        <Button onSubmit={handleFormSubmit} variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+
+    </div >
   );
 }
 
---
+++ src/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAxN1SOL95oo_SWqoX2hjl1hIRh7B5y5uw",
+    authDomain: "email-password-auth-959c3.firebaseapp.com",
+    projectId: "email-password-auth-959c3",
+    storageBucket: "email-password-auth-959c3.appspot.com",
+    messagingSenderId: "189007975603",
+    appId: "1:189007975603:web:5581267b17bfa99a8e8cc6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
